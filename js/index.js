f6aa4504688a4ec47046fce9f8750aafd08f9512
coding655 <codingmaster655@gmail.com>
2024-01-04 09:58:08 +0600

    my commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('program By-- coding655');
+document.getElementById('btn').addEventListener('click', function(){
+    const inputEmailField = document.getElementById('email-field');
+    const email = inputEmailField.value;
+    const inputPassField = document.getElementById('pass-field');
+    const pass = inputPassField.value;
+    if(email === 'imransirazee@gmail.com' && pass === 'password'){
+        window.location.href = "main-page.html";
+    }
+    else{
+        alert('Wrong Password');
+    }
+})
