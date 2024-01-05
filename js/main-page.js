78d5d4de3f08d7710c9d11b034a4da440fe9f732
coding655 <codingmaster655@gmail.com>
2024-01-05 12:44:52 +0600

    commit by coding655

--- js/main-page.js
+++ js/main-page.js
@@ -1,1 +1,25 @@
-console.log('hello sifat')
+function getInputValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValue = inputField.value;
+    const inputFieldString = parseFloat(inputFieldValue);
+    return inputFieldString;
+}
+function  getInputValueById(elementId){
+    const elementField = document.getElementById(elementId);
+    const elementFieldValue = elementField.innerText;
+    const elementFieldString = parseFloat(elementFieldValue);
+    return elementFieldString;
+}
+function setTextElementValueById(elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
+
+
+
+document.getElementById('deposit-btn').addEventListener('click', function(){
+    const depositInput = getInputValueById('deposit-input-field-id');
+    const depositElement = getInputValueById('deposit-element');
+    const TotalDepositAmount = depositInput + depositElement;
+    setTextElementValueById('deposit-element', TotalDepositAmount);
+})
